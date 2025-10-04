270d92765f8369607ee12593574036039d9011bd
superuser4 <superuser3344@protonmail.com>
2025-10-04 11:25:42 -0400

    go is serving files

--- src/main.go
+++ src/main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"log"
 	"net/http"
 )
 
 func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w,r,"./frontend/dist/index.html")
+	})
+
+	log.Println("Server starting on :8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatalf("Server failed %v", err)
+	}
 }
 
