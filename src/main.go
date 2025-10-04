5208cd116a1faae458814530054a5fc01ddb17c1
superuser4 <superuser3344@protonmail.com>
2025-10-04 15:53:11 -0400

    removed redundant img folder

--- src/main.go
+++ src/main.go
@@ -6,16 +6,13 @@ import (
 )
 
 func main() {
-	// Serve static files from frontend/dist
 	fs := http.FileServer(http.Dir("./frontend/dist"))
 	http.Handle("/assets/", fs)     // Vite bundles
 	http.Handle("/favicon.ico", fs) // optional
 
-	// API / custom routes
 	http.HandleFunc("/img/", HandleImgRoute)
 	http.HandleFunc("/imglabel", HandleImgLabelRoute)
 
-	// Fallback: always serve React's index.html
 	http.HandleFunc("/", HandleRootRoute)
 
 	log.Println("Server starting on :8080")
