18f68dce27073beeb1974842e8dec3acd4786b9f
epxy <abdelhadymohamad1@gmail.com>
2025-10-05 11:20:08 -0400

    buncha shit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,25 +5,12 @@ import ViewerPage from "./pages/ViewerPage";
 
 const App: React.FC = () => {
   return (
-    <div id="main">
-
-      <nav id = "navBar"><h1 className="title">NASA Dataset Viewer</h1></nav>
-
-      <div id="mainColumn">
-        <ZoomableImage clickHandler={outerDivClicked} src="/src/assets/test_image.jpg"/>
-
-        <div className="LabelButtons">
-          <LabelToolbar
-            onAddLabel={addLabel}
-            onDeleteLabel={deleteLabel}
-            onSaveLabels={saveLabels}
-            onClearLabels={clearLabels}
-          />
-
-          <LabelList labels={labels} onSetLabel={markLabel} onDeleteLabel={(id) => deleteSpecificLabel(id)} />
-        </div>
-      </div>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/viewer/:imgName" element={<ViewerPage />} />
+      </Routes>
+    </Router>
   );
 };
 
