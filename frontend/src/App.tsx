c7b25636b6c7561c79d3da036d41463492c00678
Kelcowvine <kelvinjin2005@gmail.com>
2025-10-05 12:55:01 -0400

    fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -130,15 +130,15 @@ const App: React.FC = () => {
       <div id="mainColumn">
         <h1 className="title">NASA Dataset Viewer</h1>
 
-        <svg width="90%" height="1%">
+        <svg width="90%" height="10px">
           <rect width="100%" height="100%" fill="#e9eaf5ff"></rect>
         </svg>
 
         <ZoomableImage clickHandler={outerDivClicked} src="/src/assets/test_image.jpg" />
 
-        <div className="InfoBox">
-          <p>Coords, The database of the image, some other info</p>
-        </div>
+        <svg width="93%" height="200px">
+          <rect width="100%" height="100%" fill="#0c0c0cff"> idk put something here </rect>
+        </svg>
       </div>
 
       <div id="mainColumn2">
