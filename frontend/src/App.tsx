bfdcd0bc649021ad89513d179e09b8371d87b7d5
CubedProgrammer <47759847+CubedProgrammer@users.noreply.github.com>
2025-10-05 15:04:21 +0000

    Fixed more crap.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-// src/App.tsx
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
@@ -6,37 +5,12 @@ import ViewerPage from "./pages/ViewerPage";
 
 const App: React.FC = () => {
   return (
-    <div id="main">
-
-      <div id="mainColumn">
-        <h1 className="title">NASA Dataset Viewer</h1>
-
-        <svg width="90%" height="10px">
-          <rect width="100%" height="100%" fill="#e9eaf5ff"></rect>
-        </svg>
-
-        <ZoomableImage clickHandler={outerDivClicked} src="/src/assets/test_image.jpg" />
-
-        <svg width="93%" height="200px">
-          <rect width="100%" height="100%" fill="#0c0c0cff"> idk put something here </rect>
-        </svg>
-      </div>
-
-      <div id="mainColumn2">
-        <div className="LabelButtons">  
-          <LabelToolbar
-            onAddLabel={addLabel}
-            onDeleteLabel={deleteLabel}
-            onSaveLabels={saveLabels}
-            onClearLabels={clearLabels}
-          />
-
-          <LabelList labels={labels} onSetLabel={markLabel} onDeleteLabel={(id) => deleteSpecificLabel(id)} />
-          </div>
-      </div>
-
-
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/viewer/:imgName" element={<ViewerPage />} />
+      </Routes>
+    </Router>
   );
 };
 
--- frontend/src/pages/ViewerPage.tsx
+++ frontend/src/pages/ViewerPage.tsx
@@ -1,4 +1,3 @@
-// src/pages/ViewerPage.tsx
 import React, { useState, useRef } from "react";
 import { useParams } from "react-router-dom";
 import ZoomableImage from "../components/ZoomableImage";
@@ -101,7 +100,7 @@ const ViewerPage: React.FC = () => {
 
   if (!imgName) return <div>No image selected</div>;
 
-  return (
+  /*return (
     <div id="body-1">
       <h1 className="title">NASA Image Labeler</h1>
 
@@ -115,15 +114,47 @@ const ViewerPage: React.FC = () => {
       </div>
 
       <div className="Map">
-        <ZoomableImage clickHandler={outerDivClicked} src={`http://localhost:8080/img/${imgName}`}
- />
-        {/* hidden crosshair checkbox */}
-        <input ref={crosshairRef} type="checkbox" id="crosshair" style={{ display: "none" }} />
+        <ZoomableImage clickHandler={outerDivClicked} src={`http://localhost:8080/img/${imgName}`}/>
+        <input ref={crosshairRef} type="checkbox" id="crosshair" />
       </div>
 
       <div className="LabelsDisplay">
         <LabelList labels={labels} onSetLabel={markLabel} onDeleteLabel={deleteSpecificLabel} />
       </div>
+    </div>
+  );*/
+  return (
+    <div id="body-1">
+
+      <div id="mainColumn">
+        <h1 className="title">NASA Dataset Viewer</h1>
+
+        <svg width="90%" height="10px">
+          <rect width="100%" height="100%" fill="#e9eaf5ff"></rect>
+        </svg>
+
+        <ZoomableImage clickHandler={outerDivClicked} src={`http://localhost:8080/img/${imgName}`}/>
+        <input ref={crosshairRef} type="checkbox" id="crosshair" />
+
+        <svg width="93%" height="200px">
+          <rect width="100%" height="100%" fill="#0c0c0cff"> idk put something here </rect>
+        </svg>
+      </div>
+
+      <div id="mainColumn2">
+        <div className="LabelButtons">  
+          <LabelToolbar
+            onAddLabel={addLabel}
+            onDeleteLabel={deleteLabel}
+            onSaveLabels={saveLabels}
+            onClearLabels={clearLabels}
+          />
+
+          <LabelList labels={labels} onSetLabel={markLabel} onDeleteLabel={(id) => deleteSpecificLabel(id)} />
+          </div>
+      </div>
+
+
     </div>
   );
 };
